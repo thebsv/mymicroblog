e8155497c81a1ade8dc64ffa3da017b4b087140c
Bhargav Srinivasan <bhargav.srinivasan92@gmail.com>
2022-05-01 21:48:09 +0530

    adding function to edit profile on the user profile page, and adding a new route for edit profile

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,8 @@ from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
 from app import app, db
-from app.forms import LoginForm
-from app.forms import RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from datetime import datetime
 
 
 @app.route('/')
@@ -75,3 +75,28 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        # The current user is fetched from the database and there
+        # is no need to call db.session.add()
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your profile was updated successfully!')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
